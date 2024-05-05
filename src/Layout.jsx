5db4e5808efc742a39ec8d7f46abe3565a5b3780
= <=>
2024-05-05 20:16:00 +0300

    5 comm

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,20 +3,34 @@ import { NavLink, Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
 import AccountSingIn from './pages/AccountEntry/AccountSingIn/AccountSingIn';
 import AccountLogin from './pages/AccountEntry/AccountLogin/AccountLogin';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 const Layout = () => {
 
    const [isOpenSingIn, setIsOpenSing] = useState(false)
    const [isOpenLogin, setIsOpenLogin] = useState(false)
-
+   console.log(SpeedInsights);
    return (
       <>
-         <Header setIsOpenLogin={setIsOpenLogin} setIsOpenSing={setIsOpenSing} />
-         {isOpenLogin && <AccountLogin setIsOpenSing={setIsOpenSing} setIsOpenLogin={setIsOpenLogin} />}
-         {isOpenSingIn && <AccountSingIn setIsOpenSing={setIsOpenSing} setIsOpenLogin={setIsOpenLogin} />}
+         <SpeedInsights />
+         <Header
+            setIsOpenLogin={setIsOpenLogin}
+            setIsOpenSing={setIsOpenSing}
+         />
+         {isOpenLogin && (
+            <AccountLogin
+               setIsOpenSing={setIsOpenSing}
+               setIsOpenLogin={setIsOpenLogin}
+            />
+         )}
+         {isOpenSingIn && (
+            <AccountSingIn
+               setIsOpenSing={setIsOpenSing}
+               setIsOpenLogin={setIsOpenLogin}
+            />
+         )}
          <Outlet />
       </>
-   )
+   );
 }
 
 export default Layout
